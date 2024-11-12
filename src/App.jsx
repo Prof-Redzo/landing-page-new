aee579fe3ef020c79f3ef0fb347874d294a6c3d5
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2024-11-12 19:46:24 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
+import { useState } from "react";
 import "./App.css";
+import { Route, Routes, useNavigate, Navigate } from "react-router-dom";
 
 function App() {
+  const [activeSection, setActiveSection] = useState(0);
+
+  const navigate = useNavigate();
+
+  const SECTIONS = [
+    {
+      id: 0,
+      name: "Home",
+      path: "/"
+    },
+    {
+      id: 1,
+      name: "About",
+      path: "/about"
+    },
+    {
+      id: 2,
+      name: "Details",
+      path: "/details"
+    },
+    {
+      id: 3,
+      name: "Contact us",
+      path: "/contact-us"
+    },
+  ];
+
+  const handleClickSection = (sectionId, path) => {
+    setActiveSection(sectionId);
+    navigate(path);
+  }
+
   return (
     <>
       <div className="navbar">
-        <div className="navbar-item">Home</div>
-        <div className="navbar-item">About</div>
-        <div className="navbar-item">Details</div>
-        <div className="navbar-item">Profile</div>
+        {SECTIONS.map((section) => (
+          <div
+            key={section.id}
+            className={`navbar-item ${
+              activeSection === section.id ? "active" : ""
+            }`}
+            onClick={() => handleClickSection(section.id, section.path) }
+          >
+            {section.name}
+          </div>
+        ))}
       </div>
+      <Routes>
+        <Route path="/" element={<h1>Home component</h1>} />
+        <Route path="/about" element={<h1>About component</h1>} />
+        <Route path="/details" element={<h1>details component</h1>} />
+        <Route path="/contact-us" element={<h1>contact component</h1>} />
+        <Route path="*" element={<Navigate to="/"/>}/>
+      </Routes>
     </>
   );
 }
